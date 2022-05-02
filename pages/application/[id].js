3ee65fb57678f10ece0ebc00f159ba643cc292c9
greatkay-olowo <oluwakayode.alao@gmail.com>
2022-05-02 01:30:08 +0100

    a

--- pages/application/[id].js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ProtectedRoute from '../../components/ProtectedRoute';
-export default function Application() {
-	return <ProtectedRoute>Application</ProtectedRoute>;
-}
