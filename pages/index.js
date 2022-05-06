7243123ab35e3a0eb987670a7b4cf6ddb45f0517
greatkay-olowo <oluwakayode.alao@gmail.com>
2022-05-06 23:44:36 +0100

    b

--- pages/index.js
+++ pages/index.js
@@ -66,9 +66,9 @@ export default function Home() {
 									viewBox='0 0 20 20'
 									xmlns='http://www.w3.org/2000/svg'>
 									<path
-										fill-rule='evenodd'
+										fillRule='evenodd'
 										d='M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z'
-										clip-rule='evenodd'></path>
+										clipRule='evenodd'></path>
 								</svg>
 							</span>
 							<h3 className='flex items-center mb-1 text-lg font-semibold  '>
@@ -102,9 +102,9 @@ export default function Home() {
 									viewBox='0 0 20 20'
 									xmlns='http://www.w3.org/2000/svg'>
 									<path
-										fill-rule='evenodd'
+										fillRule='evenodd'
 										d='M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z'
-										clip-rule='evenodd'></path>
+										clipRule='evenodd'></path>
 								</svg>
 							</span>
 							<h3 className='mb-1 text-lg font-semibold text-gray-900 dark:text-white'>
@@ -119,9 +119,9 @@ export default function Home() {
 									viewBox='0 0 20 20'
 									xmlns='http://www.w3.org/2000/svg'>
 									<path
-										fill-rule='evenodd'
+										fillRule='evenodd'
 										d='M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z'
-										clip-rule='evenodd'></path>
+										clipRule='evenodd'></path>
 								</svg>
 							</span>
 							<h3 className='mb-1 text-lg font-semibold text-gray-900 dark:text-white'>
@@ -136,9 +136,9 @@ export default function Home() {
 									viewBox='0 0 20 20'
 									xmlns='http://www.w3.org/2000/svg'>
 									<path
-										fill-rule='evenodd'
+										fillRule='evenodd'
 										d='M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z'
-										clip-rule='evenodd'></path>
+										clipRule='evenodd'></path>
 								</svg>
 							</span>
 							<h3 className='mb-1 text-lg font-semibold text-gray-900 dark:text-white'>
@@ -153,9 +153,9 @@ export default function Home() {
 									viewBox='0 0 20 20'
 									xmlns='http://www.w3.org/2000/svg'>
 									<path
-										fill-rule='evenodd'
+										fillRule='evenodd'
 										d='M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z'
-										clip-rule='evenodd'></path>
+										clipRule='evenodd'></path>
 								</svg>
 							</span>
 							<h3 className='mb-1 text-lg font-semibold text-gray-900'>
